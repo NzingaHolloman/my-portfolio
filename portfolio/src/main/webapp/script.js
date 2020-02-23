7bf25afcdd219e2319a270b4c23a5fd783d8e2ab
NzingaHolloman <you@example.com>
2020-02-23 00:23:52 -0500

    FIx comments from #1, Add background color to style.css

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,8 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
+      ['Brazil!', 'Bangkok, Thailand!', 'Accra, Ghana!', 'Botswana!','Machu Picchu!','Beijing, China!','Chichen Itza, Mexico!'];
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
