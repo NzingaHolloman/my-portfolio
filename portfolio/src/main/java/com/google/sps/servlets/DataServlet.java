3b0acf5e555f6e2ea609ef1e5f4b9b71c1fc4968
NzingaHolloman <you@example.com>
2020-03-05 07:11:12 -0500

    fixed comments from older PR

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,58 +14,54 @@
 
 package com.google.sps.servlets;
 
-import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
-import java.util.ArrayList;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    private ArrayList<String> hardcoded = new ArrayList<String>();
+  private ArrayList<String> hardcoded = new ArrayList<String>();
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
+    Query query = new Query("Comments").addSort("timestamp", SortDirection.DESCENDING);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
-    
+    ArrayList<String> comments = new ArrayList<String>();
     for (Entity entity : results.asIterable()) {
       String comment = (String) entity.getProperty("Comments:");
-      hardcoded.add(comment);
+      comments.add(comment);
     }
 
     response.setContentType("application/json;");
-    String json= new Gson().toJson(hardcoded);
-    response.getWriter().println(json); 
+    String json = new Gson().toJson(comments);
+    response.getWriter().println(json);
   }
-  
- @Override
+
+  @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String text = getParameter(request, "textInput", "default2");
-//hardcoded.add(text);
-    String json= new Gson().toJson(hardcoded);
+    String json = new Gson().toJson(hardcoded);
 
     long timestamp = System.currentTimeMillis();
-    Entity taskEntity = new Entity("Task");
+    Entity taskEntity = new Entity("Comments");
     taskEntity.setProperty("Comments:", text);
     taskEntity.setProperty("timestamp", timestamp);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(taskEntity);
 
-    //response.setContentType("application/json;");
-    //response.getWriter().println(json);
     response.sendRedirect("index.html");
   }
 
